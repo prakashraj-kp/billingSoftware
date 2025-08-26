420fe95417b41de4b34faf0ef02b45a9159f714d
1410git <prakashraj2020.pk@gamil.com>
2025-08-26 07:12:18 +0530

    Updated Explore.css styling

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,11 +1,44 @@
-const Explore =()=>{
+import './Explore.css'
+
+const Explore = () => {
+    return (
+        <div className="explore-container">
+            <div className="left-column">
+                <div className="first-row" style={{ overflowY: 'auto' }}>
+                    categories
+                </div>
+                <hr className="horizontal-line" />
+                <div className="second-row" style={{ overflowY: 'auto' }}>
+                    items
+                </div>
+            </div>
+
+            <div className="right-column d-flex flex-column">
+                <div className="customer-form-container" style={{ height: '15%' }}>
+                    customer form
+                </div>
+                <hr className="my-3 text-light" />
+                <div className="cart-item-container" style={{ height: '55%', overflowY: 'auto' }}>
+                    cart item
+                </div>
+                <div className="cart-summary-container" style={{ height: '30%' }}>
+                    cart summary
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Explore;
+
+/*const Explore =()=>{
     return(
        <div className="explore-container">
         <div className="left-column">
             <div className="first-row" style={{overflowY:'auto'}}>
                  categories
             </div>
-            <hr className="horizantal-line" />
+            <hr className="horizontal-line" />
             <div className="second-row" style={{overflowY:'auto'}}>
                  items
             </div>
@@ -18,11 +51,11 @@ const Explore =()=>{
         <div className="cart-item-container"style={{height:'55%',overflowY:'auto'}}>
             cart item
         </div>
-        </div>
         <div className="cart-summary-container" style={{height:'30%'}}>
            cart summary
+        </div>
         </div>
        </div>
     )
 }
-export default Explore;
+export default Explore;*/
