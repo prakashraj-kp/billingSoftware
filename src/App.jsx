3898895a0f637889c8820374698217774c9d8678
1410git <prakashraj2020.pk@gamil.com>
2025-09-07 11:29:58 +0530

    Add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { Route, Routes,Navigate} from 'react-router-dom'
+import { Route, Routes,Navigate, useLocation} from 'react-router-dom'
 import './App.css'
-import Menubar from './compenets/Menubar'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Explore  from './pages/Explore/Explore'
-import ManageUsers from './pages/ManageUsers/ManageUsers'
-import ManageCategory from './pages/ManageCategory/ManageCategory'
-import MangeItem from  './pages/ManageItems/ManageItems'
-import { Toaster } from 'react-hot-toast'
+import Menubar from './compenets/Menubar';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Explore  from './pages/Explore/Explore';
+import ManageUsers from './pages/ManageUsers/ManageUsers';
+import ManageCategory from './pages/ManageCategory/ManageCategory';
+import MangeItem from  './pages/ManageItems/ManageItems';
+import Login from './pages/Login/Login'; 
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-
+    const location=useLocation();
   return (
     <div>
-   <Menubar/>
+   {location.pathname !=="/login" && <Menubar/>}
    <Toaster/>
    <Routes>
     <Route path="/" element={<Navigate to="/dashboard" replace />} />
@@ -21,6 +22,7 @@ function App() {
     <Route path="/users" element={<ManageUsers/>}/>
     <Route path="/items" element={<MangeItem/>}/>
     <Route path="/explore" element={<Explore/>}/>
+    <Route path="/login" element={<Login/>}/>
    </Routes>
     </div>
   )
