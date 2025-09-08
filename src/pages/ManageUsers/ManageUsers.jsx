21bb633833b8cb0b928af30923312d529ec695b4
1410git <prakashraj2020.pk@gamil.com>
2025-09-08 19:02:47 +0530

    Added/Updated ManageUsers functionality

--- src/pages/ManageUsers/ManageUsers.jsx
+++ src/pages/ManageUsers/ManageUsers.jsx
@@ -1,14 +1,41 @@
 import './ManageUsers.css'
 import UserForm from '../../compenets/UsersForm/UserForm';
-import UsersLsit from '../../compenets/UsersList/UsersList';
+import UsersList from '../../compenets/UsersList/UsersList';
+import { useState ,useEffect} from 'react';
+import toast from 'react-hot-toast';
+import { fetchUser } from '../../Service/UserService';
+
+
 const ManageUsers=()=>{
+
+   const[users,setUsers]=useState([]);
+   const[loading,setLoading]=useState(false);
+
+   useEffect(()=>{
+      async function loadUsers() {
+         try{
+          setLoading(true);
+          const response = await fetchUser();
+          setUsers(response.data);
+         }
+         catch(error){
+           console.log(error);
+           toast.error("Unable to fetch users");
+         }
+         finally{
+            setLoading(false);
+         }
+      }
+      loadUsers();
+   },[])
+
     return(
-      <div className="users-container taxt-light">
+      <div className="users-container text-light">
          <div className="left-column">
-           <UserForm/>
+           <UserForm setUsers={setUsers}/>
         </div>
         <div className="right-column">
-           <UsersLsit/>
+           <UsersList users={users} setUsers={setUsers}/>
         </div>
     </div>
     )
