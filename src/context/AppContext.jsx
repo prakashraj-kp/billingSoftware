bf394b31af8efab80e3afe01f5249f31a9906f60
1410git <prakashraj2020.pk@gamil.com>
2025-09-08 19:01:08 +0530

    Updated AppContext: added auth state management

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,18 +1,26 @@
 import { createContext, useEffect, useState } from "react";
 import { fetchCategories } from "../Service/CetegoryService";
 export const AppContext=createContext(null);
+import { fetchItems } from "../Service/ItemService";
 
 export const AppContextProvider=(props)=>{
     const[categories,setCategories]=useState([]);
+    const[itemsData,setItemsData]=useState([]);
     const[auth,setAuth]=useState({token:null, role:null}); 
 
 
     useEffect(()=>{
         async function localData() {
+          if(localStorage.getItem("token") && localStorage.getItem("role")){
+            setAuth(
+              localStorage.getItem("token"),
+              localStorage.getItem("role")
+            );
+          }
          const response=await  fetchCategories(); 
-         console.log("Fetched categories:", response.data);
-
+         const itemResponse=await fetchItems();
          setCategories(response.data);
+         setItemsData(itemResponse.data);
         }
         localData();
     },[]);
@@ -41,7 +49,9 @@ const setAuthDate=(token,role)=>{
            categories,
            setCategories,
            auth,
-           setAuthDate
+           setAuthDate,
+           itemsData,
+           setItemsData
     }
     return <AppContext.Provider value={contextValue}>
      {props.children} 
